5cf10f2ba3efcd7a887de74bfca861ddd05738fd
André L. Maravilha <andre.maravilha@outlook.com>
2022-12-30 11:24:18 -0300

    Pequenas atualizações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,21 @@
 $(function() {
 
-    // Set sweet-scroll library
-    document.addEventListener('DOMContentLoaded', () => {
-        const scroller = new SweetScroll({});
-    }, false);
+    // Watch scroll event to update active item in navbar
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: "#navbar",
+        rootMargin: "0px 0px -25%",
+        smoothScroll: true
+    });
 
-    // Watch scroll event to update active item in menu
+    // Watch scroll and resize events to update navbar style
     let navbar = $('.navbar');
     let navbarMobile = $('.navbar-toggler');
 
-    if (navbarMobile.css('display') !== 'none') {
-        navbar.removeClass('navbar-dark navbar-transparent');
-        navbar.addClass('navbar-light navbar-colored shadow-sm');
-    }
-
-    $('body').scrollspy({
-        target: '#navbar',
-        offset: 50
-    });
-
-    $(window).on('scroll', () => {
-        if (navbarMobile.css('display') === 'none') {
+    const updateNavBarStyle = () => {
+        if (navbarMobile.css('display') !== 'none') {
+            navbar.removeClass('navbar-dark navbar-transparent');
+            navbar.addClass('navbar-light navbar-colored shadow-sm');
+        } else {
             if ($(this).scrollTop() >= navbar.height()) {
                 navbar.removeClass('navbar-dark navbar-transparent');
                 navbar.addClass('navbar-light navbar-colored shadow-sm');
@@ -29,5 +24,10 @@ $(function() {
                 navbar.addClass('navbar-dark navbar-transparent');
             }
         }
-    });
+    };
+
+    updateNavBarStyle();
+    $(window).on('resize', updateNavBarStyle);
+    $(window).on('scroll', updateNavBarStyle);
+
 });
